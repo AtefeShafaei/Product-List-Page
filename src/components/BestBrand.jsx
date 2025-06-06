8f4e26521aff729882ee58eb06d0944ab3486b1b
masi <atefeshafaeidev@gmail.com>
2025-06-06 19:13:21 +0330

    BeatBrands phase 2

--- src/components/BestBrand.jsx
+++ src/components/BestBrand.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import brand1 from "../assets/bran1.jpg";
+import brand2 from "../assets/brand2.jpg";
+import brand3 from "../assets/brand3.jpg";
+import brand4 from "../assets/brand4.jpg";
+import brand5 from "../assets/brand5.jpg";
 
 function BestBrand() {
+
+    const scrollRef = useRef(null);
+
+    const brands = [
+        {src: brand1},
+        {src: brand2},
+        {src: brand3},
+        {src: brand4},
+        {src: brand5}
+    ]
+
+    const scroll = (offset) => {
+        if (scrollRef.current) {
+          scrollRef.current.scrollBy({ left: offset, behavior: 'smooth' });
+        }
+    };
+
+
+
   return (
-    <div>BestBrand</div>
+
+
+    <div className='best-slider'>
+        <div className='container best'>
+
+            <div className='brand-card'>
+                    {brands.map(brand => (
+                        <div className='brand-item'>
+                            <img src={brand.src} alt='brand' />
+                        </div>
+                    ))}
+                
+            </div>    
+        </div>
+    </div>
   )
 }
 
-export default BestBrand
+export default BestBrand;
