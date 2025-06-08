26e0eca32b29983c3302539a3973439347774d21
masi <atefeshafaeidev@gmail.com>
2025-06-08 09:59:03 +0330

    Footer phase3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,8 +13,8 @@ function Footer() {
         <div className='footer-columns'>
 
             <div className='footer-item'>
-                <p>درباره ما</p>
                 <ul>
+                    <p>درباره ما</p>
                     <li><a href='#'>تاریخچه</a></li>
                     <li><a href='#'>فرصت های شغلی</a></li>
                     <li><a href='#'>تماس با ما</a></li>
@@ -22,8 +22,8 @@ function Footer() {
             </div>
 
             <div className='footer-item'>
-                <p>راهنمای خرید</p>
                 <ul>
+                    <p>راهنمای خرید</p>
                     <li><a href='#'>نحوه ثبت سفارش</a></li>
                     <li><a href='#'>رویه ارسال سفارش</a></li>
                     <li><a href='#'>شیوه پرداخت</a></li>
@@ -31,8 +31,17 @@ function Footer() {
             </div>
 
             <div className='footer-item'>
-                <p>خدمات مشتریان</p>
                 <ul>
+                    <p>خدمات مشتریان</p>
+                    <li><a href='#'>پرسش و پاسخ</a></li>
+                    <li><a href='#'>شرایط استفاده</a></li>
+                    <li><a href='#'>گزارش باگ</a></li>
+                </ul>
+            </div>
+
+            <div className='footer-item'>
+                <ul>
+                    <p>شبکه های اجتماعی</p>
                     <li><a href='#'>پرسش و پاسخ</a></li>
                     <li><a href='#'>شرایط استفاده</a></li>
                     <li><a href='#'>گزارش باگ</a></li>
