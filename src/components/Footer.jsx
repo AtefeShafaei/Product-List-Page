475ca6bf9aa4e8acd83ae56accaf7a07e9d5f243
masi <atefeshafaeidev@gmail.com>
2025-06-12 10:02:18 +0330

    Footer Final phase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { FaFacebookF, FaInstagram, FaTelegram, FaLinkedinIn } from "react-icons/fa";
+
 
 function Footer() {
   return (
@@ -42,12 +44,34 @@ function Footer() {
             <div className='footer-item'>
                 <ul>
                     <p>شبکه های اجتماعی</p>
-                    <li><a href='#'>پرسش و پاسخ</a></li>
-                    <li><a href='#'>شرایط استفاده</a></li>
-                    <li><a href='#'>گزارش باگ</a></li>
+
+                    <div className='items'>
+
+                        <li className='item'><a href='#'>
+                            <FaInstagram size={24} />
+                            </a>
+                        </li>
+
+                        <li className='item'><a href='#'>
+                            <FaLinkedinIn size={24} />
+                            </a>
+                        </li>
+
+                        <li className='item'><a href='#'>
+                            <FaTelegram size={24} />
+                            </a>
+                        </li>
+
+                        <li className='item'><a href='#'>
+                            <FaFacebookF size={24} />
+                            </a>
+                        </li>
+
+                    </div>
+
                 </ul>
             </div>
-    </div>
+        </div>
     </footer>
   )
 }
